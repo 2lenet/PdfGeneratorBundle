6f6e097f6011656c12368f827eb5f607ec7a4c22
joris <joris@2le.net>
2018-09-18 10:12:31 +0200

    bundle

---
+++ src/LlePdfGeneratorBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Lle\PdfGeneratorBundle;
+
+use Lle\EasyAdminPlusBundle\Acl\Compiler\TwigPathPass;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LlePdfGeneratorBundle extends Bundle
+{
+}
