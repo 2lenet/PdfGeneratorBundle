f555cccaa05fdb7ab23bf521ec13fe754b0ef71a
Matias <matias@2le.net>
2018-09-19 14:41:40 +0200

    working but doesn't recognize special characters like pipes

--- src/Generator/WordToPdfGenerator.php
+++ src/Generator/WordToPdfGenerator.php
@@ -7,6 +7,8 @@ use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpFoundation\Session\Attribute\NamespacedAttributeBag;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\Finder\Finder;
+use Dompdf\Dompdf;
 
 class WordToPdfGenerator
 {
@@ -15,6 +17,15 @@ class WordToPdfGenerator
         $phpWord = \PhpOffice\PhpWord\IOFactory::load($source);
         $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'HTML');
         $objWriter->save('test.html');
-        return new BinaryFileResponse('test');
+        $finder = new Finder();
+        $finder->in('./')->name('test.html');
+        foreach ($finder as $file) {
+            $contents = $file->getContents();
+        }
+        $dompdf = new Dompdf();
+        $dompdf->loadHtml($contents);
+        $dompdf->render();
+        $dompdf->stream();
+        return new BinaryFileResponse('~/Téléchargements/document.pdf');
     }
 }
