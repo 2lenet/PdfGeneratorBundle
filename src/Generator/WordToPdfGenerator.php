b44dcf27c3ed7fa2711c3cb3c6042b4cd4f14676
Matias <matias@2le.net>
2018-09-20 10:31:18 +0200

    bundle now works with twig prepared docx

--- src/Generator/WordToPdfGenerator.php
+++ src/Generator/WordToPdfGenerator.php
@@ -7,23 +7,25 @@ use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpFoundation\Session\Attribute\NamespacedAttributeBag;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
-use Symfony\Component\Finder\Finder;
 use Dompdf\Dompdf;
 
 class WordToPdfGenerator
 {
-    public static function wordToPdf($source)
+    private $twig;
+
+    public function __construct(\Twig_Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    public function wordToPdf($source, $params)
     {
         $phpWord = \PhpOffice\PhpWord\IOFactory::load($source);
         $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'HTML');
-        $objWriter->save('test.html');
-        $finder = new Finder();
-        $finder->in('./')->name('test.html');
-        foreach ($finder as $file) {
-            $contents = $file->getContents();
-        }
+        $objWriter->save('../templates/test.html.twig');
+        $string = $this->twig->loadTemplate('test.html.twig')->render($params);
         $dompdf = new Dompdf();
-        $dompdf->loadHtml($contents);
+        $dompdf->loadHtml($string);
         $dompdf->render();
         $dompdf->stream();
         return new BinaryFileResponse('~/Téléchargements/document.pdf');
