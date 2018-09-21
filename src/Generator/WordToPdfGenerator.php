f110c47cc0b274bf478afddcc0b4994821c8e2be
Matias <matias@2le.net>
2018-09-21 14:25:52 +0200

    parses twig to correctly print tables

--- src/Generator/WordToPdfGenerator.php
+++ src/Generator/WordToPdfGenerator.php
@@ -7,15 +7,18 @@ use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpFoundation\Session\Attribute\NamespacedAttributeBag;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Lle\PdfGeneratorBundle\Parsing\ReorganizerTwigParser;
 use Dompdf\Dompdf;
 
 class WordToPdfGenerator
 {
     private $twig;
+    private $reorganizerTwigParser;
 
-    public function __construct(\Twig_Environment $twig)
+    public function __construct(\Twig_Environment $twig, ReorganizerTwigParser $reorganizerTwigParser)
     {
         $this->twig = $twig;
+        $this->reorganizerTwigParser = $reorganizerTwigParser;
     }
 
     public function wordToPdf($source, $params)
@@ -23,6 +26,7 @@ class WordToPdfGenerator
         $phpWord = \PhpOffice\PhpWord\IOFactory::load($source);
         $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'HTML');
         $objWriter->save('../templates/test.html.twig');
+        $this->reorganizerTwigParser->parse("test.html.twig");
         $string = $this->twig->loadTemplate('test.html.twig')->render($params);
         $dompdf = new Dompdf();
         $dompdf->loadHtml($string);
---
+++ src/Parsing/ReorganizerTwigParser.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Lle\PdfGeneratorBundle\Parsing;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpFoundation\Session\Attribute\NamespacedAttributeBag;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+
+class ReorganizerTwigParser
+{
+    public function parse($fileName)
+    {
+        $finder = new Finder();
+        $finder->name($fileName);
+        foreach ($finder->in('../templates') as $file) {
+            $string = $file->getContents();
+        }
+        $string = preg_replace('#<p (.+)>{% for (.+) in (.+) %}</p>\n<table>#', "<table>\n<p>{% for $2 in $3 %}</p>" , $string);
+        $string = preg_replace('#</table>\n<p (.+)>{% endfor %}</p>#', "<p>{% endfor %}</p>\n</table>" , $string);
+        file_put_contents ( '../templates/'.$fileName , $string);
+        return new BinaryFileResponse('../templates/'.$fileName);
+    }
+}
