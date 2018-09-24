42539869d6962b73b90237f5cea96b9bc55aa417
Matias <matias@2le.net>
2018-09-24 16:39:35 +0200

    changing method to phpword templating

--- src/Generator/WordToPdfGenerator.php
+++ src/Generator/WordToPdfGenerator.php
@@ -8,10 +8,15 @@ use Symfony\Component\HttpFoundation\Session\Attribute\NamespacedAttributeBag;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Lle\PdfGeneratorBundle\Parsing\ReorganizerTwigParser;
+use PhpOffice\PhpWord\TemplateProcessor;
+use Symfony\Component\Finder\Finder;
 use Dompdf\Dompdf;
 
 class WordToPdfGenerator
 {
+
+    const ITERABLE = 'iterable';
+    const VARS = 'vars';
     private $twig;
     private $reorganizerTwigParser;
 
@@ -21,13 +26,39 @@ class WordToPdfGenerator
         $this->reorganizerTwigParser = $reorganizerTwigParser;
     }
 
+    public function handleTable($params) {
+        $templateProcessor = new TemplateProcessor('Template.docx');
+        for ($i = 1; $i <= count($params[self::ITERABLE]); $i++) {
+            foreach ($params[self::ITERABLE]['table' . $i][0] as $key => $content) {
+                $clonekey = $key;
+            }
+            $templateProcessor->cloneRow($clonekey, count($params[self::ITERABLE]['table' . $i]));
+            foreach ($params[self::ITERABLE] as $table) {
+                $k = 0;
+                foreach($table as $var) {
+                    $k++;
+                    foreach ($var as $key => $content) {
+                        $templateProcessor->setValue($key . '#' . $k, $content);
+                    }
+                }
+            }
+        }
+        $templateProcessor->saveAs('TemplateTest.docx');
+    }
+
     public function wordToPdf($source, $params)
     {
-        $phpWord = \PhpOffice\PhpWord\IOFactory::load($source);
+        if (array_key_exists(self::ITERABLE,$params)  ) {
+            $this->handleTable($params);
+        }
+        $phpWord = \PhpOffice\PhpWord\IOFactory::load('TemplateTest.docx');
         $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'HTML');
-        $objWriter->save('../templates/test.html.twig');
-        $this->reorganizerTwigParser->parse("test.html.twig");
-        $string = $this->twig->loadTemplate('test.html.twig')->render($params);
+        $objWriter->save('../templates/TemplateTest.html');
+        $finder = new Finder();
+        $finder->name('TemplateTest.html');
+        foreach ($finder->in('../templates') as $file) {
+            $string = $file->getContents();
+        }
         $dompdf = new Dompdf();
         $dompdf->loadHtml($string);
         $dompdf->render();
