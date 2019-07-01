0cc3cd272b27c02d6f9ccd11af2d942e6ef4f84b
joris <joris@2le.net>
2019-07-01 17:44:43 +0200

    show ressource

--- src/Action/ShowRessourceAction.php
+++ src/Action/ShowRessourceAction.php
@@ -11,6 +11,7 @@ use Lle\PdfGeneratorBundle\Generator\PdfGenerator;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 final class ShowRessourceAction
 {
@@ -24,7 +25,8 @@ final class ShowRessourceAction
     public function __invoke(Request $request): Response
     {
         $model = $this->pdfGenerator->getRepository()->find($request->get('id'));
-        return new BinaryFileResponse($this->pdfGenerator->getPath().$model->getPath());
+        $response = new BinaryFileResponse($this->pdfGenerator->getPath().$model->getPath());
+        return $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, $model->getPath());
     }
 
 }
